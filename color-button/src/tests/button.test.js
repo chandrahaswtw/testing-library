c8ebd9f5dec487163dfebcfa1f51f5a2d91f1ff6
chandrahaswtw <chandrahaswtw@gmail.com>
2021-07-29 18:48:49 +0530

    Initial tests

---
+++ color-button/src/tests/button.test.js
@@ -0,0 +1,17 @@
+import {render, screen} from "@testing-library/react";
+
+test("button has the initial color",()=>{
+
+})
+
+test("button has the initial text",()=>{
+
+})
+
+test("button turns to blue when clicked",()=>{
+    
+})
+
+test("button has the initial text",()=>{
+
+})
