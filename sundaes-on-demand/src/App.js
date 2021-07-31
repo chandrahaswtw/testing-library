8ec1705e0eaa2b03636b2a6484ef2742722b5417
chandrahaswtw <chandrahaswtw@gmail.com>
2021-07-31 21:45:28 +0530

    Sundaes on demand

---
+++ sundaes-on-demand/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ButtonComponent from "./components/button";
+
+function App() {
+  return (
+    <div className="App">
+        <ButtonComponent></ButtonComponent>
+    </div>
+  );
+}
+
+export default App;
